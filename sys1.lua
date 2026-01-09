3dc35ea06a3d99cd03de563d06833d2e594ec511
essprojectscripts <essproject0070@gmail.com>
2026-01-09 21:27:24 +0900

    sys1.lua

--- sys1.lua
+++ sys1.lua
@@ -1,5 +1,3 @@
---es's project의 이용 약관을 완전히 부숴버리셨군요! 이제 가서 자수하세요. 문서 읽고 다시 다짐하면 봐줄수도 있어요.
-
-workspace.Baseplate.Color = Color3.fromRGB(0, 0, 0) -- 베이스플레이트를 초록색으로 변경
+workspace.Baseplate.Color = Color3.fromRGB(0, 0, 0)
 workspace.Baseplate.Transparency = 0.5
 print("hello")
